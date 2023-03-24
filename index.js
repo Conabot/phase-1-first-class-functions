70790a520ddd7cfc738497150b54fbafa20c5968
Minh Tran <tdaimike@gmail.com>
2023-03-24 06:55:13 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction (spy){
+    spy();
+}
+
+function returnsANamedFunction() {
+    return function fn() {
+  };
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+
+    };
+}
